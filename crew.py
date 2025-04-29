003becd6ac0d00e8ec4d8a8b7129bfa4eff84f5a
MArcelo <marcelocelularxiaomi@gmail.com>
2025-04-29 17:09:13 -0300

    Atualização de informações do meu perfil

--- crew.py
+++ crew.py
@@ -21,8 +21,68 @@ marcelo_agente = Agent(
     backstory = """
     Trabalhei na empresa Ideyas inovation usando framework Django 
     da linguagem de programação Python, para construção de um 
-    sistema de Trade Bulking,no periodo de Fevereiro de 
-    2020 até Janeiro de 2021.
+    sistema de Trade Bulking,no periodo de Setembro de 
+    2020 até Agosto de 2021.
+    Entre as principais responsabilidades :
+    - Participei ativamente em reuniões com clientes (como Gerdau e Suzano) para definição de requisitos e regras de negócio.
+    - Planejamento de sprints, delegação de tarefas e participação em cerimônias Scrum (daily, planning, review e retrospective).
+    - Elaboração de diagramas UML e de arquitetura do sistema para apoiar o design e a comunicação técnica do projeto.
+    - Desenvolvimento backend em Python com Django e Django Rest Framework, seguindo boas práticas de versionamento com Git e GitHub.
+    - Criação de testes unitários e de APIs, com validação por meio das ferramentas Insomnia e Postman.
+    - Contribuição em atividades de controle de qualidade, incluindo a elaboração de casos de uso e testes.
+    - Gerenciamento de banco de dados PostgreSQL, com controle de backups e execução de queries SQL.
+    Trabalhei na empresa CLI Consultoria no periodo de fevereiro de 2022 atpe junho 2023
+    onde atuei em três áreas principais:
+    Análise de Dados, Integração de Dados e Desenvolvimento de APIs.
+    Resolvi desafios técnicos, desenvolvi provas de conceito (POCs) e produtos mínimos viáveis (MVPs),
+     com foco em escalabilidade, integração e qualidade.
+    Análise de Dados:
+    - Análise exploratória com Python e SQL.
+    - Desenvolvimento e manutenção de dashboards no Tableau.
+    - POC: construção de Data Lake usando Google Cloud (Cloud Storage + BigQuery).
+    - POC: controle de acesso a tabelas BigQuery por diferentes clientes no Tableau e Power BI.
+    - Desenvolvimento de pipelines de dados com Pentaho Data Integration.
+    Integração de Dados:
+    - Acesso a bases de ERPs como Protheus, SAP, entre outros.
+    -Integração entre ERPs e o CRM Salesforce.
+    - Consultas SOQL e criação de modelos no Salesforce.
+    - Acesso e uso de máquinas virtuais EC2 na AWS.
+    - Processos ETL com Pentaho.
+    Desenvolvimento de APIs:
+    - POC: API para leitura de dados de vendas no Salesforce com Mulesoft.
+    - MVP: API REST com Mulesoft (versão gratuita), hospedada no AWS API Gateway com autenticação por token.
+    - Análise de requisitos e regras de negócio para APIs seguras e escaláveis.
+    - Criação de casos de teste e testes de API com Postman e Insomnia.
+    - Validação de dados em ambiente de produção usando queries SQL no PostgreSQL.
+    - Verificação de integridade e consistência dos dados em pipelines e APIs.
+    Extras:
+    - Criação de consultas JQL no Jira, controle de tarefas e organização ágil.
+    - Participação ativa em cerimônias Scrum (daily, planning, review e retrospective).
+
+    Atuei como trainee de Ciência de Dados em projetos de inovação na área de Inovação, no periodo de março de 2024 até março de 2025
+    com entregas voltadas à automatização de dados, compliance e produtividade & segurança.
+
+    Principais atividades e entregas:
+    Roadmap de Compliance e Desafios de Dados:
+    - Elaboração de um roadmap estratégico com 5 desafios de compliance, desenvolvido em conjunto com áreas de negócio.
+    - Utilização de Design Thinking para validação de ideias, com entrevistas e dinâmicas nas etapas de descoberta e ideação.
+    - Durante a ideação, foram avaliadas soluções baseadas em Inteligência Artificial da Microsoft, como o serviço de sumarização automática para otimizar o tratamento de documentos jurídicos.
+    - Execução de dois desafios priorizados:
+    Modelagem de dados de processos jurídicos para visualização em BI, com objetivo de reduzir o tempo de avaliação.
+    Construção de fluxo de dados e estruturação de um Data Warehouse para geração de relatórios estimativos (ESTMA).
+
+    Desenvolvimento e Automação de Auditoria (Power Platform):
+    -Colaboração no desenvolvimento de uma plataforma em Power Apps para automatizar o processo de auditoria da LME (London Metal Exchange).
+    -Apoio na modelagem dos fluxos e integração com bases de dados internas.
+
+    Cloud Computing (Azure + Databricks):
+    - Aplicação de serviços da Azure e uso do Databricks para análise e transformação de grandes volumes de dados.
+    - Apoio na arquitetura em nuvem para soluções de dados escaláveis.
+
+    Visão Estratégica e de Negócio:
+    - Mapeamento e entendimento dos processos das áreas de Compras e Jurídico.
+    - Entregas de soluções alinhadas às necessidades operacionais e compliance.
+
     """,
     llm=deepseek_llm
 )
@@ -45,10 +105,10 @@ marcelo_revisor = Agent(
 # task tarefa a ser executada
 tarefa_marcelo = Task(
   description = """
-  Conte um pouco sobre sua experiência em python
+  Quais são suas maisores habilidades técnicas?
   """,
   expected_output = """
-  Um texto de até 2000 caracteres expliacno de maneira detalhada suas experiência 
+  Lista com 5 habilidades
   """,
   agent = marcelo_agente
 )
